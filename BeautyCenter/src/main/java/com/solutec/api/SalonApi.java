65cd56889cd260d8f2368871b44f29d6ff7fe2e3
MarianneTcd <marianne.trichard@free.fr>
2019-04-19 13:24:56 +0200

    modif SalonApi

--- BeautyCenter/src/main/java/com/solutec/api/SalonApi.java
+++ BeautyCenter/src/main/java/com/solutec/api/SalonApi.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.solutec.dao.SalonRepository;
 import com.solutec.entities.Salon;
+
 
 
 @RestController
@@ -24,5 +27,26 @@ public List<Salon> getSalon() {
 return salonRepos.findAll(); 	
 }
 
+@RequestMapping(value="/salons/{id}", method=RequestMethod.GET) 
+public Salon getSalon(@PathVariable Long id) { 
+	return salonRepos.findOne(id); 
+	}
+
+@RequestMapping(value="/salons", method=RequestMethod.POST) 
+public Salon saveSalon(@RequestBody Salon s) { 
+	return salonRepos.save(s); 
+	}
+
+@RequestMapping(value="/salons/{id}", method=RequestMethod.DELETE) 
+public boolean supSalon(@PathVariable Long id) { 
+	salonRepos.delete(id);
+	return true; 
+	}
+
+@RequestMapping(value="/salons/{id}", method=RequestMethod.PUT) 
+public Salon modifSalon(@PathVariable Long id, @RequestBody Salon s) { 
+	s.setId(id); 
+	return salonRepos.save(s); 
+	}
 
 }
