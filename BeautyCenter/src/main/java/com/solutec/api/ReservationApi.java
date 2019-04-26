2e496451eccdb15bd39f8bf0e7b9d5cb7ca7e04d
MarianneTcd <marianne.trichard@free.fr>
2019-04-26 10:56:41 +0200

    modif reservations Api

--- BeautyCenter/src/main/java/com/solutec/api/ReservationApi.java
+++ BeautyCenter/src/main/java/com/solutec/api/ReservationApi.java
@@ -119,7 +119,15 @@ public class ReservationApi {
 	public Reservations saveReservation(@RequestBody Reservations s) { 
 		return resRepos.save(s); 
 		}
-
+	
+	@RequestMapping(value="/reserv/{idsalon}/{iduser}/{idpresta}/{année}/{mois}/{jour}/{heure}/{minute}/{dureepresta}", method=RequestMethod.POST) 
+	public Reservations saveReservation2(@PathVariable Long idsalon, @PathVariable Long iduser, @PathVariable Long idpresta, @PathVariable int année, @PathVariable int mois, @PathVariable int jour, @PathVariable int heure, @PathVariable int minute, @PathVariable int dureepresta) { 
+		LocalDateTime dt = LocalDateTime.of(année,mois,jour,heure,minute); 
+		return resRepos.save( new Reservations(idsalon, iduser, idpresta, dt , dureepresta)); 
+		}
+	
+	
+	
 	@RequestMapping(value="/reserv/{id}", method=RequestMethod.DELETE) 
 	public boolean supReservation(@PathVariable Long id) { 
 		resRepos.delete(id);
