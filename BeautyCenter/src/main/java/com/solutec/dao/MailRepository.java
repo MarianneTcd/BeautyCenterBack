f153f5e5a4acf3555a2644fc7c9be6a045b77847
Jephy78 <jeanphilippelaine@outlook.fr>
2019-04-26 09:04:10 +0200

    ajout mail d'inscription

---
+++ BeautyCenter/src/main/java/com/solutec/dao/MailRepository.java
@@ -0,0 +1,11 @@
+package com.solutec.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.solutec.entities.Mail;
+
+public interface MailRepository extends JpaRepository<Mail, Long>{
+	
+	
+
+}
