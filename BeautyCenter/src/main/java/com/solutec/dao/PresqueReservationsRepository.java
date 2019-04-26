89f9739aab9ee82c36d57c62fe17bd653043ab0e
MartinLenglet <martinlenglet@gmail.com>
2019-04-26 12:53:41 +0200

    reservations qui fonctionennt

---
+++ BeautyCenter/src/main/java/com/solutec/dao/PresqueReservationsRepository.java
@@ -0,0 +1,10 @@
+package com.solutec.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.solutec.entities.PresqueReservations;
+
+public interface PresqueReservationsRepository extends JpaRepository<PresqueReservations, Long> {
+	
+
+}
